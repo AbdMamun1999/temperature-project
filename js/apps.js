34d31d388c7912749e574d8655aa7b526bd025c6
AbdMamun1999 <mamunrox@gmail.com>
2022-03-03 11:58:16 +0600

    add function for get input search value and search for ccity data

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,27 @@
-console.log('consle')
+// get city name
+const cityName = () =>{
+    // api key
+    const api_key = `84b176e47a8731f1c70d4fb02f22851d`
+    const city = document.getElementById('city-name').value; 
+    if(city == ''){
+        document.getElementById('city-name').placeholder = 'Please, Enter city name'
+    }else{
+        searchTemperature(city,api_key)
+    }
+}
+
+// search data for city
+const searchTemperature = (city,api_key) =>{
+    console.log(city,api_key)
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    fetch(url)
+    .then(res =>{
+        if(res.status >= 200 && res.status <= 299){
+            return res.json()
+        }else{
+            console.log('data not found')
+        }
+    })
+    .then(data => console.log(data))
+}
+
