5a4b47419f99028ce278103d8e94edb5853bae08
AbdMamun1999 <mamunrox@gmail.com>
2022-03-03 12:46:00 +0600

    add set inner text function and display function

--- js/apps.js
+++ js/apps.js
@@ -1,3 +1,4 @@
+document.getElementById('found-massage').style.display = 'none'
 // get city name
 const cityName = () =>{
     // api key
@@ -13,15 +14,33 @@ const cityName = () =>{
 // search data for city
 const searchTemperature = (city,api_key) =>{
     console.log(city,api_key)
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
     fetch(url)
     .then(res =>{
         if(res.status >= 200 && res.status <= 299){
             return res.json()
         }else{
-            console.log('data not found')
+            document.getElementById('found-massage').style.display = 'block'
         }
     })
-    .then(data => console.log(data))
+    .then(data => displayTemperature(data))
+    .catch( error => console.log(error))
+}
+// const errorMassage = error =>{
+//     document.getElementById('city-name').placeholder = 'Something went wrong'
+// }
+
+const setInnerText = (id,text) =>{
+    document.getElementById(id).innerText = text;
 }
 
+const displayTemperature = cityInfo =>{
+    console.log(cityInfo)
+    setInnerText('city',cityInfo.name)
+    setInnerText('temp',cityInfo.main.temp)
+    setInnerText('condition',cityInfo.weather[0].main)
+
+    const url = `http://openweathermap.org/img/wn/${cityInfo.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon')
+    imgIcon.setAttribute('src',url)
+}
